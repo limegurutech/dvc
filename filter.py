13773fd307827232099129793cd8e080c973b67a
Deepak Baweja <deepak.baweja@aexp.com>
2021-07-02 16:00:45 +0530

    dvc project

--- filter.py
+++ filter.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import yaml
+import os 
+import io
 
 params = yaml.safe_load(open("params.yaml"))['filter']
 df = pd.read_csv("weather-data/2006-04-10.csv", parse_dates=["Formatted Date"])
-rslt_df = df[df['Temperature (C)'] > params['value']]
-print(rslt_df)
+df = df[df['Temperature (C)'] > params['value']]
+df = df[['Formatted Date', 'Temperature (C)']]
+print(df)
+os.makedirs(os.path.join("output", "filter"), exist_ok=True)
+df.to_csv("output/filter/out.csv")
--- training.py
+++ training.py
@@ -1,11 +1,23 @@
 import pandas as pd
 import yaml
+import os
+import io
+import sys
+
 
 params = yaml.safe_load(open("params.yaml"))['training']
-df = pd.read_csv("weather-data/2006-04-10.csv", parse_dates=["Formatted Date"])
+data_path = sys.argv[1]+"/out.csv"
+df = pd.read_csv(data_path, parse_dates=["Formatted Date"])
 df = df.head(params['num_rows'])
+print(df)
 mean = df['Temperature (C)'].mean()
 print(mean)
+os.makedirs(os.path.join("output", "training"), exist_ok=True)
+output_training = os.path.join("output", "training", "out.tsv")
+with io.open(output_training, "w", encoding="utf8") as fd_out_training:
+    fd_out_training.write(str(mean))
+
+
 
 
 
