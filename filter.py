7d66f1cd5633ff3819dcce29241f2a4157b4aed8
Deepak Baweja <deepak.baweja@aexp.com>
2021-07-02 14:43:46 +0530

    changes

--- filter.py
+++ filter.py
@@ -4,5 +4,4 @@ import yaml
 params = yaml.safe_load(open("params.yaml"))['filter']
 df = pd.read_csv("weather-data/2006-04-10.csv", parse_dates=["Formatted Date"])
 rslt_df = df[df['Temperature (C)'] > params['value']]
-print(">>")
 print(rslt_df)
